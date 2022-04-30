3feb8cf490e23ba0160a89d07a3ff88eb23ff590
reelmza <jessemoses71@gmail.com>
2022-04-30 01:45:59 +0100

    Back event listener added

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ import spotifyLogo from "../src/images/spotify-logo-long.svg";
 import geniusLogo from "../src/images/genius-logo.svg";
 
 const App = () => {
-  window.onpopstate = function (event) {
+  window.addEventListener("beforeunload", (event) => {
     event.preventDefault();
-    window.alert("You wan leave");
-  };
+    if (window.event) {
+      if (window.event.clientX < 40 && window.event.clientY < 0) {
+        alert("Browser back button is clicked...");
+      } else {
+        alert("Browser refresh button is clicked...");
+      }
+    } else {
+      if (event.currentTarget.performance.navigation.type == 1) {
+        alert("Browser refresh button is clicked...");
+      }
+      if (event.currentTarget.performance.navigation.type == 2) {
+        alert("Browser back button is clicked...");
+      }
+    }
+  });
   // Displays
   const [lyricsPageVisible, setLyricsPageVisible] = useState(false);
 
